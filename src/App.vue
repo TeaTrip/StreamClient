6675a99784a32d4d99e98cf881e7ef0fb2a49745
Anna <valerevna_1999@mail.ru>
2021-03-26 20:49:36 +0300

    <commit_message>

--- src/App.vue
+++ src/App.vue
@@ -10,24 +10,7 @@
 
 <style lang="scss">
 #app {
-  // font-family: Avenir, Helvetica, Arial, sans-serif;
-  // -webkit-font-smoothing: antialiased;
-  // -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  // color: #3772be;
-}
-
-#nav {
-  padding: 30px;
-
-  // a {
-  //   font-weight: bold;
-  //   color: #edac28;
-
-  //   &.router-link-exact-active {
-  //     color: #42b983;
-  //   }
-  // }
 }
 
 </style>
